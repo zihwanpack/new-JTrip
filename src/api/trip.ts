7130892ad1ae97a8fe881cb8dd74631a6eeeed42
Layton <jihwan8807@naver.com>
2025-12-16 22:55:06 +0900

    feat(api): add createTripApi function for trip creation and update getMyAllTripsApi to a const

--- src/api/trip.ts
+++ src/api/trip.ts
@@ -2,8 +2,9 @@ import { httpClient } from './http/httpClient.ts';
 import type { Trip } from '../types/trip.ts';
 import TripError from '../errors/TripError.ts';
 import { AxiosError } from 'axios';
+import type { TripCreateRequest } from '../schemas/tripSchema.ts';
 
-export const getMyAllTripsApi = async (id: string): Promise<Trip[]> => {
+const getMyAllTripsApi = async (id: string): Promise<Trip[]> => {
   try {
     const { data } = await httpClient.get<Trip[]>(`/trips/user/${id}`);
     return data;
@@ -16,3 +17,19 @@ export const getMyAllTripsApi = async (id: string): Promise<Trip[]> => {
     throw new TripError('An unknown error occurred', 500);
   }
 };
+
+const createTripApi = async (trip: TripCreateRequest): Promise<Trip> => {
+  try {
+    const { data } = await httpClient.post<Trip>('/trips', trip);
+    return data;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      const status = error.response?.status ?? 500;
+      const message = error.response?.data?.message ?? error.message;
+      throw new TripError(message, status);
+    }
+    throw new TripError('An unknown error occurred', 500);
+  }
+};
+
+export { getMyAllTripsApi, createTripApi };
