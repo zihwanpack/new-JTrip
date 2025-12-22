54348e83457387f908bd6e0f4cc87bfb38e8ee6c
Layton <jihwan8807@naver.com>
2025-12-22 09:18:41 +0900

    feature(init): eventSchema 정의

---
+++ src/schemas/eventSchema.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const eventSchema = z.object({
+  eventName: z
+    .string()
+    .min(1, '이벤트 이름을 입력해주세요')
+    .max(30, '이벤트 이름은 30자 이내로 입력해주세요'),
+  location: z.string().min(1, '여행지를 선택해주세요'),
+  startDate: z.string().min(1, '출발일을 선택해주세요'),
+  endDate: z.string().min(1, '도착일을 선택해주세요'),
+  cost: z.array(
+    z.object({
+      category: z.string().min(1, '카테고리를 선택해주세요'),
+      value: z.number().min(1, '값을 입력해주세요'),
+    })
+  ),
+});
+
+export type EventFormValues = z.infer<typeof eventSchema>;
