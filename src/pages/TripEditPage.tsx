644b545f6f1b8db98ae101f474b1555026836dc0
Layton <jihwan8807@naver.com>
2026-01-13 23:52:30 +0900

    refactor: TripEditPage useSuspense 적용

--- src/pages/TripEditPage.tsx
+++ src/pages/TripEditPage.tsx
@@ -4,24 +4,19 @@ import { useEffect, useState } from 'react';
 import { tripFormSchema, type TripFormValues } from '../schemas/tripSchema.ts';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { FullscreenLoader } from '../components/common/FullscreenLoader.tsx';
 import { tripQueryKeys } from '../constants/queryKeys.ts';
 import { getTripDetailApi } from '../api/trip.ts';
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { useTripDetailQueryOptions } from '../hooks/query/trip.ts';
 import type { Trip } from '../types/trip.ts';
+
 export const TripEditPage = () => {
   const navigate = useNavigate();
   const { tripId } = useParams();
   const tripIdNumber = Number(tripId);
   const [step, setStep] = useState(1);
 
-  const {
-    data: tripDetail,
-    isPending: isTripDetailPending,
-    isError: isTripDetailError,
-    error: tripDetailError,
-  } = useQuery<Trip | null>({
+  const { data: tripDetail } = useSuspenseQuery<Trip | null>({
     queryKey: tripQueryKeys.detail(tripIdNumber),
     queryFn: () => getTripDetailApi({ id: tripIdNumber }),
     ...useTripDetailQueryOptions({ id: tripIdNumber }),
@@ -56,14 +51,6 @@ export const TripEditPage = () => {
       createdBy: tripDetail.createdBy,
     });
   }, [tripDetail, form]);
-
-  if (isTripDetailPending) {
-    return <FullscreenLoader />;
-  }
-
-  if (isTripDetailError) {
-    return <div>에러가 발생했습니다: {tripDetailError?.message}</div>;
-  }
 
   const handleCloseForm = () => {
     navigate(`/trips/${tripId}`);
